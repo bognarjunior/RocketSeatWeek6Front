d5beecbaf324e44dc8dc08b55468a45f5113a11e
Bognar Junior <bognar_junior@yahoo.com.br>
2019-04-13 13:22:14 -0300

    Create Main component
    Criando o componente Main

---
+++ src/pages/Main/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './styles.css';
+
+export default class Main extends Component {
+  render() {
+    return (
+      <div id="main-container">
+        
+      </div>
+    )
+  }
+}
