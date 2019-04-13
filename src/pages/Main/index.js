ca97b1771bf7a949887edd69c700b47c20979f20
Bognar Junior <bognar_junior@yahoo.com.br>
2019-04-13 14:29:14 -0300

    Create api.js
    Creiando o arquivo de conexão com a api

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
+import api from '../../services/api';
+
 import logo from '../../assets/logo.svg';
 import './styles.css';
 
+
 export default class Main extends Component {
+	state = {
+		newBox: ''
+	};
+
+	handleSubmit = (e) => {
+		e.preventDefault();
+		console.log('box', this.state.newBox);
+	}
+
+	handleInputChange = (e) => {
+		this.setState({	newBox: e.target.value });
+	}
+
   render() {
     return (
       <div id="main-container">
-        <form action="">
+        <form onSubmit={this.handleSubmit}>
 					<img src={logo} alt=""></img>
-					<input type="text" placeholder="Criar um box" />
+					<input
+						value={this.state.newBox}
+						type="text" 
+						placeholder="Criar um box"
+						onChange={this.handleInputChange}
+					/>
 					<button type="submit">Criar</button>
 				</form>
       </div>
