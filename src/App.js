12806db85a79a65f252cd647f48ed51d478c38d8
Bognar Junior <bognar_junior@yahoo.com.br>
2019-04-13 13:50:17 -0300

    Create form in Main and custom css
    Criando o formulário no main e customizando o css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
+import Main from './pages/Main';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-       <h1>Hello world</h1>
-      </div>
+      <Main />
     );
   }
 }
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import logo from '../../assets/logo.svg';
 import './styles.css';
 
 export default class Main extends Component {
   render() {
     return (
       <div id="main-container">
-        
+        <form action="">
+					<img src={logo} alt=""></img>
+					<input type="text" placeholder="Criar um box" />
+					<button type="submit">Criar</button>
+				</form>
       </div>
     )
   }
