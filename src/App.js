f68c8d5771a87b690d0ad30707aeae9df9e2965e
Bognar Junior <bognar_junior@yahoo.com.br>
2019-04-13 15:51:53 -0300

    Create routes
    Criando as rotas da aplicação

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
-import Main from './pages/Main';
+import Routes from './routes';
 
-class App extends Component {
-  render() {
-    return (
-      <Main />
-    );
-  }
-}
+const App = () => <Routes />;
 
 export default App;
